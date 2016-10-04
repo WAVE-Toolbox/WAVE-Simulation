c9757e0d02057b601107342dd80f235d9f4a7163
Florian Wittkamp <florian.wittkamp@kit.edu>
2016-10-04 10:02:27 +0200

    Changes Configuration Comment prefix to #
    This allows to read file paths, which include the / character.

--- src/Configuration.hpp
+++ src/Configuration.hpp
@@ -18,167 +18,15 @@ class Configuration
 {
 public:
     
-    /*! \brief Default constructor
-     \param filename Path to configuration file
-     */
-	Configuration( std::string filename )
-	{
-		// read all lines in file
-
-	    std::string line;
-	    std::map<std::string,std::string> map;
-	    std::ifstream input( filename.c_str() );
-
-	    while ( std::getline( input, line ) )
-	    {
-	        size_t lineEnd = line.size();
-	    	std::string::size_type commentPos1 = line.find_first_of( "/", 0 );
-	        if ( std::string::npos != commentPos1 )
-	        {
-	        	std::string::size_type commentPos2 = line.find_first_of( "/", commentPos1 );
-	        	if ( std::string::npos != commentPos2 )
-	        	{
-	        		if( commentPos1 == 0 )
-	        		{
-	        			continue;
-	        		}
-	        		lineEnd = commentPos1;
-	        	}
-	        }
-
-	    	std::string::size_type equalPos = line.find_first_of( "=", 0 );
-
-	        if ( std::string::npos != equalPos )
-	        {
-	            // tokenize it  name = val
-	            std::string name = line.substr( 0, equalPos );
-	            size_t len = lineEnd - ( equalPos + 1);
-	            std::string val  = line.substr( equalPos + 1, len);
-	            map.insert( std::pair<std::string,std::string>( name, val ) );
-	        }
-	    }
-	    input.close();
-
-		// check map and assign all values with right "cast" to members
-		size_t nArgs = 18;
-		if ( map.size() != nArgs )
-		{
-			std::cout << filename << " does not include a valid configutation with " << nArgs << " arguments." << std::endl;
-		}
-        
-		std::istringstream( map[ "NZ" ] ) >> NZ; // IndexType
-		std::istringstream( map[ "NX" ] ) >> NX; // IndexType
-		std::istringstream( map[ "NY" ] ) >> NY; // IndexType
-
-		std::istringstream( map[ "DH" ] ) >> DH; // IndexType
-
-		std::istringstream( map[ "DT" ] ) >> DT; // ValueType
-		std::istringstream( map[ "T" ] ) >> T;  // ValueType
-
-        std::istringstream( map[ "ReadModel" ] ) >> ReadModel; // IndexType
-        std::istringstream( map[ "FilenameModel" ] ) >> FilenameModel; // std::string
-    
-		std::istringstream( map[ "velocity" ] ) >> velocity; // ValueType
-		std::istringstream( map[ "rho" ] ) >> rho; // ValueType
-
-		std::istringstream( map[ "fc" ] ) >> fc; // IndexType
-		std::istringstream( map[ "amp" ] ) >> amp; // IndexType
-
-		std::istringstream( map[ "source_z" ] ) >> source_z; // IndexType
-		std::istringstream( map[ "source_x" ] ) >> source_x; // IndexType
-		std::istringstream( map[ "source_y" ] ) >> source_y; // IndexType
-
-		std::istringstream( map[ "seismogram_z" ] ) >> seismogram_z; // IndexType
-		std::istringstream( map[ "seismogram_x" ] ) >> seismogram_x; // IndexType
-		std::istringstream( map[ "seismogram_y" ] ) >> seismogram_y; // IndexType
-
-	    // calculate other parameters
-
-	    N = NZ * NX * NY;
-
-	    M = velocity * velocity * rho; // P-wave modulus
-
-	    NT = static_cast<IndexType>( ( T / DT ) + 0.5 ); // MATLAB round(T/DT)
-
-	    v_factor = lama::Scalar(DT / DH);
-	    p_factor = lama::Scalar(DT);
-
-	    source_index     = index( source_x,     source_y,     source_z,     NX, NY, NZ );
-	    seismogram_index = index( seismogram_x, seismogram_y, seismogram_z, NX, NY, NZ );
-        
-        FilenameModel="model/"+FilenameModel;
-
-	}
-
     /*! \brief Default deconstructor
      */
 	~Configuration(){}
-
-    /*! \brief Print configuration
-     */
-	void print()
-	{
-	    IndexType velocity_max = velocity; // TODO: is velocity a vector?
-	    double courant = velocity_max * DT / DH;
-	    IndexType wavelength = velocity_max / fc;
-
-	    std::cout << "Configuration:" << std::endl;
-	    std::cout << "Criteriums:" << std::endl;
-	    std::cout << "    Wavelength: " << wavelength << " m" << std::endl;
-	    std::cout << "    Gridpoints per Wavelength: " << wavelength / DH << std::endl;
-	    std::cout << "    Courant-number: " << courant << std::endl;
-	    if ( courant >= 0.8 )
-	    {
-	        std::cout << "Simulation will be UNSTABLE" << std::endl;
-	        std::cout << "Choose smaller DT, eg.: " << DH * 0.3 / velocity_max << std::endl;
-	        exit(0);
-	    }
-	    std::cout << "Modelling-domain:" << std::endl;
-	    std::cout << "    Z: " << DH * NZ << " m (Depth)" << std::endl;
-	    std::cout << "    X: " << DH * NX << " m (Horizontal)" << std::endl;
-        std::cout << "    Y: " << DH * NY << " m (Horizontal)" << std::endl;
-        std::cout << "Material:" << std::endl;
-        if(ReadModel==1) {
-            std::cout << "    Model will be read in from disk" << std::endl;
-            std::cout << "    First Lame-Parameter: " << FilenameModel << ".pi.mtx" << std::endl;
-            std::cout << "    Density: " << FilenameModel << ".density.mtx" << std::endl;
-        } else {
-            std::cout << "    A homogeneous model will be generated" << std::endl;
-            std::cout << "    Velocity:" << velocity << " m/s" << std::endl;
-            std::cout << "    Density:" << rho << " g/cm3" << std::endl;
-        }
-	    std::cout << std::endl;
-	}
-
-    /*! \brief Print all parameter (for debugging)
-     */
-	void printAllRaw()
-	{
-		std::cout << "NZ=" << getNZ() << std::endl;
-		std::cout << "NX=" << getNX() << std::endl;
-		std::cout << "NY=" << getNY() << std::endl;
-		std::cout << "DH=" << getDH() << std::endl;
-		std::cout << "DT=" << getDT() << std::endl;
-		std::cout << "T=" << getT() << std::endl;
-		std::cout << "velocity=" << getVelocity() << std::endl;
-		std::cout << "rho=" << getRho() << std::endl;
-		std::cout << "fc=" << getFC() << std::endl;
-		std::cout << "amp=" << getAMP() << std::endl;
-		std::cout << "source_z=" << getSourceZ() << std::endl;
-		std::cout << "source_x=" << getSourceX() << std::endl;
-		std::cout << "source_y=" << getSourceY() << std::endl;
-		std::cout << "seismogram_z=" << getSeismogramZ() << std::endl;
-		std::cout << "seismogram_x=" << getSeismogramX() << std::endl;
-		std::cout << "seismogram_y=" << getSeismogramX() << std::endl;
-		std::cout << "N=" << getN() << std::endl;
-		std::cout << "M=" << getM() << std::endl;
-		std::cout << "NT=" << getNT() << std::endl;
-		std::cout << "v_factor=" << getVfactor() << std::endl;
-		std::cout << "p_factor=" << getPfactor() << std::endl;
-		std::cout << "source_index=" << getSourceIndex() << std::endl;
-		std::cout << "seismogram_index=" << getSeismogramIndex() << std::endl;
-	}
-
+    
+    Configuration( std::string filename );
+    
+    void print();
+    void printAllRaw();
+    
 	IndexType getNZ() { return NZ; } ///< Return NZ (Depth)
 	IndexType getNX() { return NX; } ///< Return NX
 	IndexType getNY() { return NY; } ///< Return NY
@@ -281,4 +129,162 @@ private:
 	IndexType source_index;  ///< Position of source in 1D coordinates
 	IndexType seismogram_index;  ///< Position of receiver in 1D coordinates
 };
+
+
+
+template<typename ValueType>
+Configuration<ValueType>::Configuration( std::string filename )
+{
+    // read all lines in file
+    
+    std::string line;
+    std::map<std::string,std::string> map;
+    std::ifstream input( filename.c_str() );
+    
+    while ( std::getline( input, line ) )
+    {
+        size_t lineEnd = line.size();
+        std::string::size_type commentPos1 = line.find_first_of( "#", 0 );
+        if ( std::string::npos != commentPos1 )
+        {
+            std::string::size_type commentPos2 = line.find_first_of( "#", commentPos1 );
+            if ( std::string::npos != commentPos2 )
+            {
+                if( commentPos1 == 0 )
+                {
+                    continue;
+                }
+                lineEnd = commentPos1;
+            }
+        }
+        
+        std::string::size_type equalPos = line.find_first_of( "=", 0 );
+        
+        if ( std::string::npos != equalPos )
+        {
+            // tokenize it  name = val
+            std::string name = line.substr( 0, equalPos );
+            size_t len = lineEnd - ( equalPos + 1);
+            std::string val  = line.substr( equalPos + 1, len);
+            map.insert( std::pair<std::string,std::string>( name, val ) );
+        }
+    }
+    input.close();
+    
+    // check map and assign all values with right "cast" to members
+    size_t nArgs = 18;
+    if ( map.size() != nArgs )
+    {
+        std::cout << filename << " does not include a valid configutation with " << nArgs << " arguments." << std::endl;
+        }
+        
+        std::istringstream( map[ "NZ" ] ) >> NZ; // IndexType
+        std::istringstream( map[ "NX" ] ) >> NX; // IndexType
+        std::istringstream( map[ "NY" ] ) >> NY; // IndexType
+        
+        std::istringstream( map[ "DH" ] ) >> DH; // IndexType
+        
+        std::istringstream( map[ "DT" ] ) >> DT; // ValueType
+        std::istringstream( map[ "T" ] ) >> T;  // ValueType
+        
+        std::istringstream( map[ "ReadModel" ] ) >> ReadModel; // IndexType
+        std::istringstream( map[ "FilenameModel" ] ) >> FilenameModel; // std::string
+        
+        std::istringstream( map[ "velocity" ] ) >> velocity; // ValueType
+        std::istringstream( map[ "rho" ] ) >> rho; // ValueType
+        
+        std::istringstream( map[ "fc" ] ) >> fc; // IndexType
+        std::istringstream( map[ "amp" ] ) >> amp; // IndexType
+        
+        std::istringstream( map[ "source_z" ] ) >> source_z; // IndexType
+        std::istringstream( map[ "source_x" ] ) >> source_x; // IndexType
+        std::istringstream( map[ "source_y" ] ) >> source_y; // IndexType
+        
+        std::istringstream( map[ "seismogram_z" ] ) >> seismogram_z; // IndexType
+        std::istringstream( map[ "seismogram_x" ] ) >> seismogram_x; // IndexType
+        std::istringstream( map[ "seismogram_y" ] ) >> seismogram_y; // IndexType
+        
+        // calculate other parameters
+        
+        N = NZ * NX * NY;
+        
+        M = velocity * velocity * rho; // P-wave modulus
+        
+        NT = static_cast<IndexType>( ( T / DT ) + 0.5 ); // MATLAB round(T/DT)
+        
+        v_factor = lama::Scalar(DT / DH);
+        p_factor = lama::Scalar(DT);
+        
+        source_index     = index( source_x,     source_y,     source_z,     NX, NY, NZ );
+        seismogram_index = index( seismogram_x, seismogram_y, seismogram_z, NX, NY, NZ );
+        
+}
+
+
+/*! \brief Print all parameter (for debugging)
+ */
+template<typename ValueType>
+void Configuration<ValueType>::printAllRaw()
+{
+    std::cout << "NZ=" << getNZ() << std::endl;
+    std::cout << "NX=" << getNX() << std::endl;
+    std::cout << "NY=" << getNY() << std::endl;
+    std::cout << "DH=" << getDH() << std::endl;
+    std::cout << "DT=" << getDT() << std::endl;
+    std::cout << "T=" << getT() << std::endl;
+    std::cout << "velocity=" << getVelocity() << std::endl;
+    std::cout << "rho=" << getRho() << std::endl;
+    std::cout << "fc=" << getFC() << std::endl;
+    std::cout << "amp=" << getAMP() << std::endl;
+    std::cout << "source_z=" << getSourceZ() << std::endl;
+    std::cout << "source_x=" << getSourceX() << std::endl;
+    std::cout << "source_y=" << getSourceY() << std::endl;
+    std::cout << "seismogram_z=" << getSeismogramZ() << std::endl;
+    std::cout << "seismogram_x=" << getSeismogramX() << std::endl;
+    std::cout << "seismogram_y=" << getSeismogramX() << std::endl;
+    std::cout << "N=" << getN() << std::endl;
+    std::cout << "M=" << getM() << std::endl;
+    std::cout << "NT=" << getNT() << std::endl;
+    std::cout << "v_factor=" << getVfactor() << std::endl;
+    std::cout << "p_factor=" << getPfactor() << std::endl;
+    std::cout << "source_index=" << getSourceIndex() << std::endl;
+    std::cout << "seismogram_index=" << getSeismogramIndex() << std::endl;
+}
+
+/*! \brief Print configuration
+ */
+template<typename ValueType>
+void Configuration<ValueType>::print()
+{
+    IndexType velocity_max = velocity; // TODO: is velocity a vector?
+    double courant = velocity_max * DT / DH;
+    IndexType wavelength = velocity_max / fc;
+    
+    std::cout << "Configuration:" << std::endl;
+    std::cout << "Criteriums:" << std::endl;
+    std::cout << "    Wavelength: " << wavelength << " m" << std::endl;
+    std::cout << "    Gridpoints per Wavelength: " << wavelength / DH << std::endl;
+    std::cout << "    Courant-number: " << courant << std::endl;
+    if ( courant >= 0.8 )
+    {
+        std::cout << "Simulation will be UNSTABLE" << std::endl;
+        std::cout << "Choose smaller DT, eg.: " << DH * 0.3 / velocity_max << std::endl;
+        exit(0);
+    }
+    std::cout << "Modelling-domain:" << std::endl;
+    std::cout << "    Z: " << DH * NZ << " m (Depth)" << std::endl;
+    std::cout << "    X: " << DH * NX << " m (Horizontal)" << std::endl;
+    std::cout << "    Y: " << DH * NY << " m (Horizontal)" << std::endl;
+    std::cout << "Material:" << std::endl;
+    if(ReadModel==1) {
+        std::cout << "    Model will be read in from disk" << std::endl;
+        std::cout << "    First Lame-Parameter: " << FilenameModel << ".pi.mtx" << std::endl;
+        std::cout << "    Density: " << FilenameModel << ".density.mtx" << std::endl;
+    } else {
+        std::cout << "    A homogeneous model will be generated" << std::endl;
+        std::cout << "    Velocity:" << velocity << " m/s" << std::endl;
+        std::cout << "    Density:" << rho << " g/cm3" << std::endl;
+    }
+    std::cout << std::endl;
+}
 
