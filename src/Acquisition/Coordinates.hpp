11b62e4ea6af4c2ff4b152190826d14b0e8b1c33
Florian Wittkamp <florian.wittkamp@kit.edu>
2016-11-07 12:45:37 +0100

    Modified error message in coordiante to be more precisly

--- src/Acquisition/Coordinates.hpp
+++ src/Acquisition/Coordinates.hpp
@@ -141,7 +141,7 @@ IndexType KITGPI::Acquisition::Coordinates<ValueType>::map3Dcoordinate2index(Ind
 {
     if ( Z >= NZ || X >= NX || Y >= NY || Z < 0 || X < 0 || Y < 0 )
     {
-        COMMON_THROWEXCEPTION ( "Could not map from coordinate to indize!" )
+        COMMON_THROWEXCEPTION ( "Could not map from coordinate to index!\nGiven indix: X=" << X << ", Y=" << Y << ", Z=" << Z << " is located outside of the grid which runs from 0 to (NX-1), 0 to (NY-1) and 0 to (NZ-1)!" )
         return -100;
     }
     else
