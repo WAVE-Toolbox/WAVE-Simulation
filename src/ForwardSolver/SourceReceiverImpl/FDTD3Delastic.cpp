1120f80be13dbd3bc7f07f81342cff51c7884dbf
Tilman Steinweg <tilman.metz@kit.edu>
2018-06-04 13:58:43 +0200

    corrected output of pressure seismograms
    pressure is p=-1/3 (sxx+syy+szz)
    
    also in the acoustic case the wavefield p represents the main stresses
    so seismograms are written out with seismoP=-wavefieldP

--- src/ForwardSolver/SourceReceiverImpl/FDTD3Delastic.cpp
+++ src/ForwardSolver/SourceReceiverImpl/FDTD3Delastic.cpp
@@ -23,6 +23,7 @@ void KITGPI::ForwardSolver::SourceReceiverImpl::FDTD3Delastic<ValueType>::gather
     gatherSeismogram_samplesPressure.gather(Sxx, coordinates, common::BinaryOp::COPY);
     gatherSeismogram_samplesPressure.gather(Syy, coordinates, common::BinaryOp::ADD);
     gatherSeismogram_samplesPressure.gather(Szz, coordinates, common::BinaryOp::ADD);
+    gatherSeismogram_samplesPressure*=-1/3;
     seismogramDataPressure.setColumn(gatherSeismogram_samplesPressure, t, common::BinaryOp::COPY);
 }
 
