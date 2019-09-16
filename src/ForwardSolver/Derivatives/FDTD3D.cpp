471f7be02af5077e2dffcef13af126c6d18f0cef
Mark <mark.wienoebst@student.kit.edu>
2019-09-16 16:22:24 +0200

    bugfix, Dyf only not needed when free surface and variable grid are used at the same time

--- src/ForwardSolver/Derivatives/FDTD3D.cpp
+++ src/ForwardSolver/Derivatives/FDTD3D.cpp
@@ -276,7 +276,7 @@ void KITGPI::ForwardSolver::Derivatives::FDTD3D<ValueType>::initializeMatrices(s
     DzfSparse.scale(this->DT);
     DzbSparse.scale(this->DT);
 
-    if (useFreeSurface != 1) {
+    if (!((useFreeSurface == 1) && (useVarGrid))){
         HOST_PRINT(comm, "", "Dyf,\n");
         this->calcDyf(modelCoordinates, dist);
         DyfSparse.setContextPtr(ctx);
