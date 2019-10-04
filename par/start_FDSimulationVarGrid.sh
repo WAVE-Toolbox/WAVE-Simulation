4dbe4d609a250c35ac54d70dbba5b70430c1d28f
Tilman Steinweg <tilman.metz@kit.edu>
2019-10-04 14:58:43 +0200

    finished merge cleanup

--- par/start_FDSimulationVarGrid.sh
+++ par/start_FDSimulationVarGrid.sh
@@ -4,4 +4,4 @@ export SCAI_UNSUPPORTED=IGNORE
 
 rm -rf seismograms/seismogram.p.mtx
 export OMP_NUM_THREADS=1
-mpirun -n 4  ./../installation/bin/SOFI "configuration/mconfigurations/configuration_s305x305x305_vg1_gp1_fd0_geoKmeans_28.txt"
+mpirun -np 4  ./../build/bin/SOFI "configuration/configurationVarGrid.txt"
